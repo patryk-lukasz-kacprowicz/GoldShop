81584a6bcc965ae8a1a44858ef3eec928ceeb1a8
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2024-02-25 21:34:47 +0100

    Create relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
@@ -31,5 +33,9 @@ class Product extends Model
 
     public function assignedTo(): BelongsTo {
         return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function categories(): BelongsToMany {
+        return $this->belongsToMany(Category::class, 'product_categories');
     }
 }
