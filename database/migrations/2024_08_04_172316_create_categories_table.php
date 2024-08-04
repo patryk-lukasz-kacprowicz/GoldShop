37a9748d99748733d9688c271c35d50428f6cb8b
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2024-08-04 17:50:42 +0200

    Create migration for categories

---
+++ database/migrations/2024_08_04_172316_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Category::class, 'parent_id')->nullable();
+            $table->string('title', 255)->unique();
+            $table->string('slug', 255)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
